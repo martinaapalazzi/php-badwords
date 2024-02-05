665bb84c1452f5ecb0e65efc544defd94ad4c29e
martinaapalazzi <martinaapalazzi@gmail.com>
2024-02-05 16:39:41 +0100

    finished

--- about.php
+++ about.php
@@ -8,3 +8,28 @@
     $censured = str_replace($badWord, '***', $userText);
     echo $censured;
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <p>
+        <?php
+            echo $userText;
+            echo 'length usertext'.": ".strlen($userText);
+        ?>
+    </p>
+    <p>
+        <?php
+            echo $censured;
+            echo 'length badword'.": ".strlen($badWord);
+        ?>
+    </p>
+
+</body>
+</html>
